9260e60ab509015d9b3cb041c3e80413a94c9d57
IanDuman-ag <kirbydumz@gmail.com>
2025-09-22 11:14:33 +0800

    Add files via upload

---
+++ Dumz/endpoint/login.php
@@ -0,0 +1,39 @@
+include ('../conn/conn.php');
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $stmt = $conn->prepare("SELECT `password` FROM `tbl_user` WHERE `username` = :username");
+    $stmt->bindParam(':username', $username);
+    $stmt->execute();
+
+    if ($stmt->rowCount() > 0) {
+        $row = $stmt->fetch();
+        $stored_password = $row['password'];
+
+        // Use password_verify to check hashed password
+        if (password_verify($password, $stored_password)) {
+            echo "
+            <script>
+                alert('Login Successfully!');
+                window.location.href = 'http://localhost/dumz/home.php';
+            </script>
+            "; 
+        } else {
+            echo "
+            <script>
+                alert('Login Failed, Incorrect Password!');
+                window.location.href = 'http://localhost/dumz/login.php';
+            </script>
+            ";
+        }
+    } else {
+        echo "
+            <script>
+                alert('Login Failed, User Not Found!');
+                window.location.href = 'http://localhost/dumz/login.php';
+            </script>
+            ";
+    }
+}
